4a49bf55eab4aa2e0e0e55725e2aee5d40b44c1c
Sangwoo Shin <ssangwoo96@gmail.com>
2024-12-04 17:21:41 +0900

    과제 완료

---
+++ src/index.js
@@ -0,0 +1,13 @@
+"use strict";
+//string
+const a = "";
+//number
+const n = 1;
+//boolean
+const b = true;
+//null
+const l = null;
+//any
+let y = 5;
+y = "";
+y = true;
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+//string
+const a: string = ""
+
+//number
+const n: number = 1
+
+//boolean
+const b: boolean = true
+
+//null
+const l: null = null
+
+//any
+let y: any = 5
+y = ""
+y = true
